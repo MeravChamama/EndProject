c60a90d0148cd33fae7130a913cf4c0e47ce6f47
meravchamama@gmail.com <meravchamama@gmail.com>
2020-06-21 19:33:30 +0300

    server

---
+++ ServerProject/DTO/CustomerDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class CustomerDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Fax { get; set; }
+        public Nullable<bool> IsBorrower { get; set; }
+        public bool IsReliable { get; set; }
+    }
+}
